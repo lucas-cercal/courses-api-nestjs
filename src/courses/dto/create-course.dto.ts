089868cda59a9ada3e2b4717754239508283d82f
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-01-24 11:14:14 -0300

    validating the data received by the app

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, ArrayUnique } from 'class-validator';
+
 export class CreateCourseDTO {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString({ each: true })
+  @ArrayUnique()
   readonly tags: string[];
 }
