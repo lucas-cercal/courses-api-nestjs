bc543b7d8e3bf508eac65c4bbd55d0913b5cd970
Lucas Cercal Fontes <lucascercal.fontes@gmail.com>
2024-05-07 15:57:10 -0300

    add create courses tags table migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1715105994666 } from 'src/migrations/1715105994666-CreateCoursesTable';
 import { CreateTagsTable1715107181985 } from 'src/migrations/1715107181985-CreateTagsTable';
+import { CreateCoursesTagsTable1715107979495 } from 'src/migrations/1715107979495-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1715105994666, CreateTagsTable1715107181985],
+  migrations: [
+    CreateCoursesTable1715105994666,
+    CreateTagsTable1715107181985,
+    CreateCoursesTagsTable1715107979495,
+  ],
 });
