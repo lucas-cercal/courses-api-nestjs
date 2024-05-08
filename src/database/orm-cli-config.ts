07ef3422d7ab48c6dfed18847fc6e6c67f36912b
Lucas Cercal Fontes <lucascercal.fontes@gmail.com>
2024-05-08 14:25:09 -0300

    add coursesid to courses tags table migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -4,6 +4,7 @@ import { CreateCoursesTable1715105994666 } from 'src/migrations/1715105994666-Cr
 import { CreateTagsTable1715107181985 } from 'src/migrations/1715107181985-CreateTagsTable';
 import { CreateCoursesTagsTable1715107979495 } from 'src/migrations/1715107979495-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTagsTable1715110467309 } from 'src/migrations/1715110467309-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1715178940823 } from 'src/migrations/1715178940823-AddTagsIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
     CreateTagsTable1715107181985,
     CreateCoursesTagsTable1715107979495,
     AddCoursesIdToCoursesTagsTable1715110467309,
+    AddTagsIdToCoursesTagsTable1715178940823,
   ],
 });
